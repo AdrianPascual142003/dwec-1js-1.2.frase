2ac46fe6d630aab2479884c4220ba79d888d39fe
adrianpascual142003 <adrianpascual142003@gmail.com>
2022-09-19 18:15:48 +0200

    [ADD] Practica acabada

--- scripts/functions.js
+++ scripts/functions.js
@@ -1,31 +1,42 @@
 'use strict'
 
 function letras(cadena) {
-
+	return cadena.length;
 }
 
 function palabras(cadena) {
-
+	let palabrasContadas = cadena.split(' ');
+	return palabrasContadas.length;
 }
 
 function maysc(cadena) {
-
+	return cadena.toLocaleUpperCase();
 }
 
 function titulo(cadena) {
-
+	cadena = cadena.split(' ');
+	for (let index in cadena) {
+		cadena[index] = cadena[index].charAt(0).toUpperCase() + cadena[index].substr(1);
+	}
+	return cadena.join(' ');
 }
 
 function letrasReves(cadena) {
-
+	return cadena.split('').reverse().join('');
 }
 
 function palabrasReves(cadena) {
-
+	return cadena.split(' ').reverse().join(' ');
 }
 
 function palindromo(cadena) {
-
+	let cadenaReves = cadena.split(' ');
+	for (let index in cadenaReves) {
+		cadenaReves[index] = letrasReves(cadenaReves[index]);
+	}
+	cadenaReves = cadenaReves.join(' ');
+	cadenaReves = palabrasReves(cadenaReves);
+	return cadena.toLocaleLowerCase().replaceAll(' ','') == cadenaReves.toLocaleLowerCase().replaceAll(' ','');
 }
 
 module.exports = {
--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,32 @@
 'use strict'
 
+let cadena = prompt("Introduce una frase:");
+
+let numFrase = cadena => {
+    if (cadena == null) {
+        throw Error("Has de introducir una cadena");
+    }else if (!isNaN(cadena)) {
+        throw Error("Has de introducir una cadena de texto");
+    }else {
+        console.log("Frase: \"" + cadena + "\"");
+        console.log(letras(cadena) + " letras y " + palabras(cadena) + " palabras");
+        console.log(maysc(cadena));
+        console.log(titulo(cadena));
+        console.log(letrasReves(cadena));
+        console.log(palabrasReves(cadena));
+        if (palindromo(cadena)) {
+            console.log("Si es un palindromo");
+        }else {
+            console.log("No es un palindromo");
+        }
+    }
+}
+
+try {
+    numFrase(cadena);
+}catch (error) {
+    console.error(error);
+}
+
+
 
